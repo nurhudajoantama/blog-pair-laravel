135bb807be16474e9eb033328cd01f6e5196bb41
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-06-26 09:02:00 +0700

    add validation on create page

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -2,29 +2,35 @@
 
 @section('content')
 
-<h1>Create Blog</h1>
+<h1 class="mt-3 mb-5">Create Blog</h1>
 
 <form action="{{route('blogs.store')}}" method="POST">
     @csrf
     <div class="form-group">
+        @error('slug')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+            placeholder="Title" value="{{old('title')}}">
+        @error('title')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="body">Body</label>
-        <textarea class="form-control" id="body" name="body" rows="3"></textarea>
+        <textarea class="form-control @error('title') is-invalid @enderror" id="body" name="body"
+            rows="3">{{old('body')}}</textarea>
+        @error('body')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-
-    @error('title')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    @error('body')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    @error('slug')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
 </form>
 
 @endsection
