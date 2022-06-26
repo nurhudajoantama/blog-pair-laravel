b66e96d02fa1636cca1d22620c6f65534faebdf3
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-06-26 09:52:49 +0700

    make faker seeder
    Co-authored-by: RaihanPutra <RaihanPutra@users.noreply.github.com>

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->unique()->sentence(3);
+        $slug = Str::slug($title);
+        $body = $this->faker->paragraph;
+        $excerpt = Str::limit(strip_tags($body, 35));
+        return compact('title', 'slug', 'excerpt', 'body');
+    }
+}
