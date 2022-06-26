aea6e469dc47bd31013fc76bbb08d544ef1ffcde
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-06-26 08:33:26 +0700

    add comfirmation delete n fix popper js

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -22,11 +22,37 @@
             <td>
                 <div class="d-flex">
                     <a href="{{route('blogs.edit', compact('blog'))}}" class="btn btn-primary btn-sm mr-2">Edit</a>
-                    <form action="{{route('blogs.delete', compact('blog'))}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                    </form>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                        data-bs-target="#deleteModal{{$blog->id}}">
+                        Delete
+                    </button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="deleteModal{{$blog->id}}" tabindex="-1"
+                        aria-labelledby="deleteModal{{$blog->id}}" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteModalTitle{{$blog->id}}">Detele Post</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close">X</button>
+                                </div>
+                                <div class="modal-body">
+                                    Are You Sure want to delete post with title <strong>{{ $blog->title }}</strong>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">No</button>
+                                    {{-- <button type="button" class="btn btn-danger">Yes</button> --}}
+                                    <form action="{{route('blogs.delete', compact('blog'))}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </td>
         </tr>
