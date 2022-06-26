36ac6b2de5516e62163ed014d21219f119f768be
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-06-26 10:07:05 +0700

    formatting date index
    Co-authored-by: RaihanPutra <RaihanPutra@users.noreply.github.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,11 +13,11 @@
 
 <div class="row mt-4">
     @foreach ($blogs as $blog)
-    <div class="col-4 mb-3">
+    <div class="col-md-4 mb-3">
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title pb-2">{{$blog->title}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted"></h6>
+                <small class="card-subtitle mb-2 text-muted">{{$blog->created_at->diffForHumans() }}</small>
                 <p class="card-text">{{$blog->excerpt}}</p>
                 <a href="{{route('blogs.show', $blog)}}" class="card-link">Read More.</a>
             </div>
