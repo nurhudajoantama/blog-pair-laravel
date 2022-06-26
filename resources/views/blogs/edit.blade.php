b892dff534fc11478d6860db3a2c82e36f760828
nurhudajoantama <nurhuda.joantama14@gmail.com>
2022-06-26 10:43:00 +0700

    add validation blogs edit
    Co-authored-by: RaihanPutra <RaihanPutra@users.noreply.github.com>

--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -8,24 +8,30 @@
     @csrf
     @method('PUT')
     <div class="form-group">
+        @error('slug')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Title" value='{{ $blog->title }}'>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+            placeholder="Title" value="{{ old('title', $blog->title) }}">
+        @error('title')
+        <div class=" invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="body">Body</label>
-        <textarea class="form-control" id="body" name="body" rows="3">{{ $blog->body }}</textarea>
+        <textarea class="form-control" id="body" name="body" rows="3">{{ old('body', $blog->body) }}</textarea>
+        @error('body')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 
-    @error('title')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    @error('body')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    @error('slug')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
 </form>
 
 @endsection
